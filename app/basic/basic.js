a00ce623f0dc9e9c3f021c4ed425a249ed1717f3
Lukasz Wisniewski <wluk1000@gmail.com>
2015-05-31 02:19:06 +0200

    volume steering

--- app/basic/basic.js
+++ app/basic/basic.js
@@ -19,5 +19,18 @@ angular.module('pidioApp.basic', ['ngRoute'])
             });
         }
 
+        $scope.setVolume = function() {
+            var volume = $scope.volume;
+            $http.post(addr + '/volume/' + volume).success(function(data) {
+                $scope.action = "volume: " + volume
+            })
+        }
+        
+
         $scope.simple('status');
+
+        $http.get(addr + '/volume').success(function(volume) {
+            $scope.volume = volume;
+        })
+
     }]);
